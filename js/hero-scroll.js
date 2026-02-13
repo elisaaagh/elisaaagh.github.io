b18f0c7a437f36e272d09c7d447387d4495a5e34
Elisa <71036868+elisaaagh@users.noreply.github.com>
2026-02-13 09:57:54 -0600

    Update hero-scroll.js

--- js/hero-scroll.js
+++ js/hero-scroll.js
@@ -32,3 +32,13 @@ window.onbeforeunload = function () {
 window.addEventListener("load", function() {
   window.scrollTo(0, 0);
 });
+
+window.addEventListener("scroll", function() {
+  const nav = document.querySelector("nav");
+
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
